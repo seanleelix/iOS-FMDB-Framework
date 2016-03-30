b9a52188aad82c0444e135903c31750f304353de
Sean <seanleelix@gmail.com>
2016-03-30 18:04:25 +0800

    Init Project

--- FMDBFramework/ViewController.m
+++ FMDBFramework/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DatabaseOperation.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    DatabaseOperation *databaseOperation = [[DatabaseOperation alloc] init];
+    [databaseOperation addPersonWithName:@"test" height:1.7f];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # iOS-FMDB-Framework
 iOS FMDB Framework with Android Style
 
-1.#pod init  
+1.$pod init  
 2.edit podfile -> pod 'FMDB/SQLCipher'  
-3.#pod install  
+3.$pod install  
